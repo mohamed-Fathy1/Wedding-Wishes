81d0651c351e6eefe93bb23b3b07d94e29def203
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2025-01-26 06:05:09 +0200

    Add credit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import { Toaster } from "sonner";
 import WeddingWishes from "../components/wedding-wishes";
+import { Heart } from "lucide-react";
 
 export default function Home() {
   return (
     <main>
       <WeddingWishes />
       <Toaster dir="rtl" position="bottom-right" />
-      <div className="text-center my-10">
-        <span className="text-[0.800rem] text-gold-dark">
-          Made with ❤️ by Mohamed Fathy
+      <div className="flex justify-center text-center my-10">
+        <span className="flex items-center gap-2 text-[0.700rem] text-gold-dark">
+          Made with
+          <Heart className="w-4 h-4 text-roseGold-600" fill="currentColor" />
+          by Mohamed Fathy
         </span>
       </div>
     </main>
